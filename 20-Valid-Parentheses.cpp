ca8f3909e61602e16e3f94ed6a499ee037ae1ad5
Mahedi Hasan <59446877+Mahedi-hasan1@users.noreply.github.com>
2025-01-31 20:41:27 +0600

    [20] [Time Beats: 100.00%] [Memory Beats: 61.46%] - LeetPush

---
+++ 20-Valid-Parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i=0; i<s.size() ; i++){
+
+            if(s[i]==')' && st.size()>=1 && st.top()=='(')
+                st.pop();
+            else if(s[i]=='}' && st.size()>=1 && st.top()=='{')
+                st.pop();
+            else if(s[i]==']' && st.size()>=1 && st.top()=='[')
+                st.pop();
+            else st.push(s[i]);
+        }
+        return st.empty() ;
+    }
+};
