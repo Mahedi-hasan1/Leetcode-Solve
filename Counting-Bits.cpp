e825c79e2daf0e14a0a5917f4a2ae1f01801526c
Mahedi Hasan <59446877+Mahedi-hasan1@users.noreply.github.com>
2025-10-30 22:13:05 +0600

    [338] [Time Beats: 100.00%] [Memory Beats: 97.43%] - LeetPush

--- Counting-Bits.cpp
+++ Counting-Bits.cpp
@@ -4,7 +4,8 @@ public:
         vector<int>res(n+1,0);
         for(int i=1; i<=n; i++){
             if(i%2==0)res[i]= res[i/2]; // x*2 just adds one 0 in x representation.
-            else res[i]= res[i-1]+1;  // adds one set bit
+            else res[i]= res[i-1]+1;  
+            // adds one set bit
         }
         return res;
     }
